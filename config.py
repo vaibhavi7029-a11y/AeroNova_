e5b19fa82350446a1b8e490a7a83d6195da68f3d
Vaibhavi Sutar <vaibhavi7029@gmail.com>
2026-08-06 21:15:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 import streamlit as st
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-if "OPENWEATHER_API_KEY" in st.secrets:
-    OPENWEATHER_API_KEY = st.secrets["OPENWEATHER_API_KEY"]
-else:
-    OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+OPENWEATHER_API_KEY = st.secrets["OPENWEATHER_API_KEY"]
 
 BASE_URL = "https://api.openweathermap.org/data/2.5"
