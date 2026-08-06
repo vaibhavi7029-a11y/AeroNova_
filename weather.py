7ea141824ca8f0b5003c3f72ba902732962d2297
Vaibhavi Sutar <vaibhavi7029@gmail.com>
2026-08-06 07:40:00 +0530

    Refactor weather fetching to use new API structure

--- weather.py
+++ weather.py
@@ -8,76 +8,43 @@ class Weather:
         self.api_key = WEATHER_API_KEY
 
     def get_weather(self, city):
-        """
-        Get current weather + 7-day forecast + AQI
-        """
 
-        url = f"{BASE_URL}/forecast.json"
+        url = f"{BASE_URL}/weather"
 
         params = {
-            "key": self.api_key,
             "q": city,
-            "days": 7,
-            "aqi": "yes",
-            "alerts": "yes"
+            "appid": self.api_key,
+            "units": "metric"
         }
 
         try:
-            response = requests.get(url, params=params, timeout=15)
+            response = requests.get(url, params=params, timeout=10)
+
+            data = response.json()
 
             if response.status_code != 200:
                 return {
                     "success": False,
-                    "message": response.json().get(
-                        "error", {}
-                    ).get(
-                        "message",
-                        "Unable to fetch weather."
-                    )
+                    "message": data.get("message", "Unable to fetch weather.")
                 }
-
-            data = response.json()
 
             return {
                 "success": True,
-
-                # ------------------------
-                # Location
-                # ------------------------
-
-                "city": data["location"]["name"],
-                "region": data["location"]["region"],
-                "country": data["location"]["country"],
-                "lat": data["location"]["lat"],
-                "lon": data["location"]["lon"],
-                "localtime": data["location"]["localtime"],
-
-                # ------------------------
-                # Current Weather
-                # ------------------------
-
-                "temperature": data["current"]["temp_c"],
-                "feels_like": data["current"]["feelslike_c"],
-                "condition": data["current"]["condition"]["text"],
-                "icon": "https:" + data["current"]["condition"]["icon"],
-                "humidity": data["current"]["humidity"],
-                "wind": data["current"]["wind_kph"],
-                "wind_dir": data["current"]["wind_dir"],
-                "pressure": data["current"]["pressure_mb"],
-                "visibility": data["current"]["vis_km"],
-                "uv": data["current"]["uv"],
-                "air_quality": data["current"].get("air_quality", {}),
-                "last_updated": data["current"]["last_updated"],
-
-                # ------------------------
-                # Forecast
-                # ------------------------
-
-                "forecast": data["forecast"]["forecastday"]
+                "city": data["name"],
+                "country": data["sys"]["country"],
+                "lat": data["coord"]["lat"],
+                "lon": data["coord"]["lon"],
+                "temperature": data["main"]["temp"],
+                "feels_like": data["main"]["feels_like"],
+                "humidity": data["main"]["humidity"],
+                "pressure": data["main"]["pressure"],
+                "condition": data["weather"][0]["main"],
+                "description": data["weather"][0]["description"],
+                "icon": f"https://openweathermap.org/img/wn/{data['weather'][0]['icon']}@2x.png",
+                "wind": data["wind"]["speed"]
             }
 
         except Exception as e:
-
             return {
                 "success": False,
                 "message": str(e)
