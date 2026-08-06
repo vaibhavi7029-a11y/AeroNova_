703607459c02a9178d246143acff5683591eece6
Vaibhavi Sutar <vaibhavi7029@gmail.com>
2026-08-06 07:36:43 +0530

    Rename WEATHER_API_KEY to OPENWEATHER_API_KEY

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 load_dotenv()
 
-WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 BASE_URL = "https://api.openweathermap.org/data/2.5"
